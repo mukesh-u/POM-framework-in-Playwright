2c92323bb9ef0172db6d1411d9cae6ca3903ca3b
mukesh.u <mukesh.u@media.net>
2026-06-26 16:59:06 +0530

    testing cron job functionality

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,5 +16,5 @@ test('login & add to cart test', async({page})=>{
     const cartPage=await productPage.clickOnAddToCartButton();
     const orderSummarypage=await cartPage.clickOnCheckout();
     await expect(orderSummarypage.summaryTitleLocator).toBeVisible();
-    console.log('Testing cron job')
+    console.log('Testing cron job functionality')
 })
